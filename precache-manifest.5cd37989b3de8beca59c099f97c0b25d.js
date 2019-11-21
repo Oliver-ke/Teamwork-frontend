123b1ce2c9271205bd169d8031023e46df15c122
azorji kelechi oliver <kelechioliver96@gmail.com>
2019-11-21 07:47:20 +0100

    Updates

---
+++ precache-manifest.5cd37989b3de8beca59c099f97c0b25d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010c7f9d2077a7aedc4433908b592e28",
+    "url": "/Teamwork-frontend/index.html"
+  },
+  {
+    "revision": "e34e5cfc8ea16086a8f5",
+    "url": "/Teamwork-frontend/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1a13c589b6e703c7a352",
+    "url": "/Teamwork-frontend/static/js/2.eaecfdc2.chunk.js"
+  },
+  {
+    "revision": "e34e5cfc8ea16086a8f5",
+    "url": "/Teamwork-frontend/static/js/main.2fdf44e7.chunk.js"
+  },
+  {
+    "revision": "3293e7a2a84834e70148",
+    "url": "/Teamwork-frontend/static/js/runtime-main.47c63ec7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Teamwork-frontend/static/media/logo.25bf045c.svg"
+  }
+]);
